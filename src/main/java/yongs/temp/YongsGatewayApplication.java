286b645680eb9cbfa0a127c63059bd3f63e83108
yongs <yongs@DESKTOP-GATTK1M>
2019-08-06 20:13:53 +0900

    최초생성

---
+++ src/main/java/yongs/temp/YongsGatewayApplication.java
@@ -0,0 +1,19 @@
+package yongs.temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@SpringBootApplication
+public class YongsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YongsGatewayApplication.class, args);
+	}
+
+}
